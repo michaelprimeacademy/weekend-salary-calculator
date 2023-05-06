36b28b00b6c9f87775232fc1c8e131f741f9f4c9
Michael Karas <michael_primeacademy@karas1.com>
2023-05-06 19:05:34 -0400

    starting over AGAIN

---
+++ script.js
@@ -0,0 +1,15 @@
+//  input form
+const form = document.getElementById("form");
+
+// submit the form
+function submitForm() {
+  // submit data
+  let firstName = document.getElementById("firstName").value;
+  let lastName = document.getElementById("lastName").value;
+  let idNumber = document.getElementById("idNumber").value;
+  let jobTitle = document.getElementById("jobTitle").value;
+  let annualSalary = document.getElementById("annualSalary").value;
+  console.log(jobTitle);
+}
+
+//console.log(submitForm());
